221481f520aa1d639d58ec91cded520254e07ffd
Hassan Afzaal <99679950+hassanUleth@users.noreply.github.com>
2022-07-04 12:12:46 -0600

    Setup a tasks page

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BlazorDeploymentTest;
+using BlazorDeploymentTest.Shared.ViewModels;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<TasksViewModel>();
 
 await builder.Build().RunAsync();
---
+++ Shared/ViewModels/TasksViewModel.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BlazorDeploymentTest.Shared.ViewModels
+{
+    public partial class TasksViewModel : BaseViewModel
+    {
+        [ObservableProperty] 
+        private string buttonText = "Click here to add task";
+
+        [ObservableProperty]
+        private string currentTask;
+
+        [ObservableProperty]
+        private List<string> tasks = new List<string>();
+
+        [RelayCommand]
+        public void AddTask()
+        {
+            this.Tasks.Add(currentTask);
+            currentTask = string.Empty;
+        }
+    }
+}
